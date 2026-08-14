eb9fb3c48dac8fa1f99f5401666280e3f9e1f40f
Henrique Furtini <furtinidev@gmail.com>
2026-08-14 09:55:17 -0300

    feat: get location latitude and longitude

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import argparse
+import json
 import requests 
 import os
 from dotenv import load_dotenv
+from geopy.geocoders import Nominatim
+import time
+from pprint import pprint
 
 load_dotenv()
-GEOCODING_API = os.getenv("GEOCODING_API")
+WEATHER_API = os.getenv("WEATHER_API")
 
 parser = argparse.ArgumentParser(description="Displays the day weather summary")
 parser.add_argument("--city", help="select the city to display the weather")
 
 args = parser.parse_args()
 
-url = "http://api.openweathermap.org/geo/1.0/direct?q=" + args.city + "&appid=" + GEOCODING_API
-response = requests.get(url)
+app = Nominatim(user_agent="coordinates")
+location = app.geocode(args.city).raw
+latitude = location["lat"]
+longitude = location["lon"]
 
-print(response.json())
-print(args.city)
+url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={WEATHER_API}"
+response = requests.get(url)
+json_awnser = response.json()
+print(json_awnser)
