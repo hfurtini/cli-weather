eb8ba9e6e22b298f355d7158dd78c781e334348b
Henrique Furtini <furtinidev@gmail.com>
2026-08-14 23:31:46 -0300

    feat: unit choice system

---
+++ src/unit_conversor.py
@@ -0,0 +1,9 @@
+def kelvin_to_celsius(temperature):
+    celsius = temperature - 273.15
+    return round(celsius,2)
+
+
+def kelvin_to_fahrenheit(temperature):
+    fahrenheit = ((temperature - 273.15) * 9)/5 + 32
+    return round(fahrenheit, 2)
+
