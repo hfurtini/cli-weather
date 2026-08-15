63c22b35ab8825150b6df937da3ea2047a9f7090
Henrique Furtini <furtinidev@gmail.com>
2026-08-14 23:31:12 -0300

    feat: unit choice system

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import requests 
 import os
+from unit_conversor import kelvin_to_celsius, kelvin_to_fahrenheit
 from dotenv import load_dotenv
 from geopy.geocoders import Nominatim
 from jsonpath_ng import jsonpath, parse
@@ -11,6 +12,7 @@ WEATHER_API = os.getenv("WEATHER_API")
 
 parser = argparse.ArgumentParser(description="Displays the day weather summary")
 parser.add_argument("--city", help="select the city to display the weather")
+parser.add_argument("--unit", help="select the unity to display the temperatures")
 
 args = parser.parse_args()
 
@@ -28,18 +30,19 @@ temperature = parse("$.main.temp")
 matches = [match.value for match in temperature.find(json_awnser_formatted)]
 print("Displaying informations for " + args.city +":\n")
 for match in matches:
-    print(f"Temperature: {match}")
-
-print("\n")
+    if args.unit == "metric": print(f"Temperature: {kelvin_to_celsius(match)} °C")
+    elif args.unit == "imperial": print(f"Temperature: {kelvin_to_fahrenheit(match)} °F")
+    else: print(f"Temperature: {match} K")
 
 temperature_sensation = parse("$.main.feels_like")
 matches = [match.value for match in temperature_sensation.find(json_awnser_formatted)]
 for match in matches:
-    print(f"Temperature sensation: {match}")
-
-print("\n")
+    if args.unit == "metric": print(f"Temperature: {kelvin_to_celsius(match)} °C")
+    elif args.unit == "imperial": print(f"Temperature: {kelvin_to_fahrenheit(match)} °F")
+    else: print(f"Temperature: {match} K")
 
 weather_summary = parse("$.weather[*].description")
 matches = [match.value for match in weather_summary.find(json_awnser_formatted)]
 for match in matches:
     print(f"Weather description: {match}")
+print("\n")
