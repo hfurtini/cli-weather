1f3efb567f2d03b92892c205f2ad904afbbe1fb2
Henrique Furtini <furtinidev@gmail.com>
2026-08-15 10:41:51 -0300

    feat: create a json for a default city

---
+++ src/cache.py
@@ -0,0 +1,23 @@
+import json
+import os
+from jsonpath_ng import parse
+def create_city_cache(city, file_name="default.json"):
+    data = {
+        "city": city
+    }
+    json_data = json.dumps(data)
+    if os.path.exists(file_name):
+        os.remove("default.json")
+        with open(file_name, "w") as file:
+            file.write(json_data)
+            file.close
+    else:
+        with open(file_name, "w") as file:
+            file.write(json_data)
+            file.close
+
+def read_city_cache():
+    with open("default.json", "r") as json_file:
+        content = json.load(json_file)
+        city = content['city']
+        return city
